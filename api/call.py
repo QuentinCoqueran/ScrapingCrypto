dcfbd771fcef237df2f673c307418b2e3b1b99d2
QuentinCoqueran <coqueranquentin@gmail.com>
2022-01-26 21:51:40 +0100

    wip refactor call to class

--- api/call.py
+++ api/call.py
@@ -2,22 +2,25 @@
 
 import requests
 import json
+class ApiCalls : 
+
+    api_url = "https://api.coingecko.com/api/v3/"
+
+    def __init__(self, API_URL) :
 
 
-API_URL = "https://api.coingecko.com/api/v3/"
+    def get_vs_currencies(self) :
+        """
+        Recupère les devises de comparaison
+        
+        Appel API retourne la liste des devises
+        """
 
-def get_vs_currencies() :
-    """
-    Recupère les devises de comparaison
-    
-    Appel API retourne la liste des devises
-    """
-
-    url = "{}simple/supported_vs_currencies".format(API_URL)
-    try :
-        response = requests.get(url)
-        content = json.loads(response.content.decode('utf-8'))
-        return content
-    except requests.exceptions.RequestException :
-        raise
+        url = "{}simple/supported_vs_currencies".format(self.api_url)
+        try :
+            response = requests.get(url)
+            content = json.loads(response.content.decode('utf-8'))
+            return content
+        except requests.exceptions.RequestException :
+            raise
 
