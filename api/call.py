975045c91d13a69a61f231d431cf1f8fd8c3d3b6
QuentinCoqueran <coqueranquentin@gmail.com>
2022-01-26 21:47:28 +0100

    ajout getvs_currencies

--- api/call.py
+++ api/call.py
@@ -1,6 +1,23 @@
 # coding: utf-8
 
 import requests
+import json
 
-COIN_GEKO_API_URL = "https://api.coingecko.com/api/v3/"
+
+API_URL = "https://api.coingecko.com/api/v3/"
+
+def get_vs_currencies() :
+    """
+    Recupère les devises de comparaison
+    
+    Appel API retourne la liste des devises
+    """
+
+    url = "{}simple/supported_vs_currencies".format(API_URL)
+    try :
+        response = requests.get(url)
+        content = json.loads(response.content.decode('utf-8'))
+        return content
+    except requests.exceptions.RequestException :
+        raise
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # coding: utf-8
+import api.call
 
 def main():
+    print(api.call.get_vs_currencies())
     pass
 
 if __name__ == '__main__':
