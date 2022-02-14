14de589263666e574238699db8b904f9ed42e6be
Shan AZIZ <shan.azizh@gmail.com>
2022-02-14 21:55:53 +0100

    build : test in main

--- main.py
+++ main.py
@@ -2,10 +2,23 @@
 
 from app.api.RequestManager import RequestManager
 from app.App import App
+from app.model.Coin import Coin
+from app.model.Currency import Currency
+from app.model.Report import Report
 
 
 def main():
     request_manager: RequestManager = RequestManager("https://api.coingecko.com/api/v3/")
+    coin = Coin("bitcoin", "Bitcoin", "BTC", 1)
+    coin2 = Coin("ethereum", "Ethereum", "Ether", 1)
+    currency = Currency("eur")
+    currency2 = Currency("usd")
+
+    coins = [coin, coin2]
+    currencies = [currency, currency2]
+    report = Report("mon rapport", coins, currencies)
+    print(report)
+
     app = App(request_manager)
     app.start()
 
