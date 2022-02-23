bb1dd68d2d3ce86f354b3c23d9f22e165235faba
Shan AZIZ <shan.azizh@gmail.com>
2022-02-23 23:54:31 +0100

    build : save_all + refactor

--- app/model/Report.py
+++ app/model/Report.py
@@ -12,9 +12,5 @@ class Report:
         self.currencies.append(currency)
 
     def __str__(self):
-        string = "{{ \"name\": \"{}\", \"coins\" : [{}], \"currencies\": [{}]}}" \
-            .format(
-            self.name,
-            (', '.join([str(x) for x in self.coins])),
-            (', '.join([str(x) for x in self.currencies])))
+        string = f'{{ "name": "{self.name}", "coins" : [{(", ".join([str(x) for x in self.coins]))}], "currencies": [{(", ".join([str(x) for x in self.currencies]))}]}}'
         return string
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 # coding: utf-8
 
-from app.api.request_manager import RequestManager
 from app.App import App
-from app.model.Coin import Coin
-from app.model.Currency import Currency
-from app.model.Report import Report
 
 
 def main():
@@ -14,13 +10,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# coin = Coin("bitcoin", "Bitcoin", "BTC", 1)
-# coin2 = Coin("ethereum", "Ethereum", "Ether", 1)
-# currency = Currency("eur")
-# currency2 = Currency("usd")
-#
-# coins = [coin, coin2]
-# currencies = [currency, currency2]
-# report = Report("mon rapport", coins, currencies)
-# print(report)
